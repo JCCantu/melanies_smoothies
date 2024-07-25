f011ea6bbbcd801a883b6d601d385d641df043b2
JCCantu <47751018+JCCantu@users.noreply.github.com>
2024-07-25 12:34:16 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,6 +22,8 @@ ingredients_list = st.multiselect('Choose up to 5 ingridients:',my_dataframe,max
 
 if ingredients_list: 
     ingredients_string = ''
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+    fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
     
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
@@ -39,6 +41,5 @@ if ingredients_list:
         st.success('Your Smoothie is ordered, '+ name_on_order +'!', icon="✅")
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
+
+
